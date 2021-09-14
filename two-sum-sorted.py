e567ed2d8b4a7a916c99d8ab609ee3370a998b0f
Alec Jones <alecaej2002@gmail.com>
2021-09-13 21:47:37 -0700

    solution for a sorted two sum, uses less memory than 98% of submissions

---
+++ two-sum-sorted.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        pointer1 = 0
+        pointer2 = len(numbers) - 1
+        while pointer1 < pointer2:
+            numSum = numbers[pointer1] + numbers[pointer2]
+            if numSum == target:
+                return [pointer1 + 1, pointer2 + 1]
+            elif numSum > target:
+                pointer2 -= 1
+            else: 
+                pointer1 += 1
