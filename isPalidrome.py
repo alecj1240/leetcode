2c2f065f6ef0f64b81fa10cdc7b12f3dafdfa3ab
Alec Jones <alecaej2002@gmail.com>
2021-09-13 22:57:06 -0700

    much simpler to read; using string method

--- isPalidrome.py
+++ isPalidrome.py
@@ -4,18 +4,7 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x < 0:
+        if str(x) == str(x)[::-1]:
+            return True
+        else:
             return False
-        
-        arr = [int(d) for d in str(x)]
-        pointer1 = 0
-        pointer2 = len(arr) - 1
-        
-        while pointer1 < pointer2:            
-            if arr[pointer1] == arr[pointer2]:
-                pointer1 += 1
-                pointer2 -= 1
-            else:
-                return False
-            
-        return True
